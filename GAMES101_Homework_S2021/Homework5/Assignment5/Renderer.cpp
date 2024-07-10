39aff22cc44447e461cde1abc2708dfac5a00dd7
MolgyWSL <z1186786523@qq.com>
2024-07-10 12:30:27 +0800

    作业5

--- GAMES101_Homework_S2021/Homework5/Assignment5/Renderer.cpp
+++ GAMES101_Homework_S2021/Homework5/Assignment5/Renderer.cpp
@@ -228,9 +228,12 @@ void Renderer::Render(const Scene& scene)
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
-            // x (horizontal) variable with the *imageAspectRatio*            
+            // x (horizontal) variable with the *imageAspectRatio*
+            x = (((float)i / (float)scene.width) * 2 - 1) * scale * imageAspectRatio;
+            y = - (((float)j / (float)scene.height) * 2 - 1) * scale;
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
